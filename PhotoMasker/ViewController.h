aca57deb0fee58807c11a5cf7fe171c1cc8a8c6c
Andrew.Leo <anderw2007@foxmail.com>
2016-11-03 11:55:13 +0800

    Initial Commit

---
+++ PhotoMasker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoMasker
+//
+//  Created by Andrew Leo on 03/11/2016.
+//  Copyright © 2016 Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
